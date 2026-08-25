97fb991073b8c9c4bac5b24c9499502872629964
Archer N <kingdedede2019@gmail.com>
2026-08-25 15:29:41 -0500

    Lecture 1

---
+++ lecture1.py
@@ -0,0 +1,18 @@
+# Archer Niemi 8-25-2026
+# Chapter 1: Demo
+
+# Printing
+# Printing allows us to see output on the terminal
+print("hello world")
+
+# The newline character \n move the output to a new line
+print("first line\nsecond line")
+print("third line")
+
+# Output multiple variables and multiple values
+weight = 400
+print("Weight is", weight, "Tons!")
+
+# We can get input with the input() function
+name = input("Enter your name: ")
+print("Hello", name)
