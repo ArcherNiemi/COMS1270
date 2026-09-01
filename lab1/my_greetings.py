67b78c7be84afa72abd53be0079441ad77a62b9b
Archer N <kingdedede2019@gmail.com>
2026-09-01 14:22:08 -0500

    First lab done

---
+++ lab1/my_greetings.py
@@ -0,0 +1,9 @@
+# Archer Niemi    8/31/2026
+# Gradescope test
+
+def print_greeting(name):
+    print(f"Hello, {name}!")
+
+def return_greeting(name):
+    greeting = "Hello, " + name + "!"
+    return greeting
---
+++ lab1/my_math.py
@@ -0,0 +1,8 @@
+# Archer Niemi    8/31/2026
+# Gradescope test
+
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
